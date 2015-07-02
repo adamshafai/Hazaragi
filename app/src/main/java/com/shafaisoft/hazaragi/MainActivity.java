0bc66f781cbe50d7b63f391a635f3760d819dc75
shafai <shafai86@gmail.com>
2015-07-02 11:07:25 +0200

    1.1

--- app/src/main/java/com/shafaisoft/hazaragi/MainActivity.java
+++ app/src/main/java/com/shafaisoft/hazaragi/MainActivity.java
@@ -5,23 +5,37 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-
+import android.widget.ImageView;
+import android.widget.ImageButton;
 public class MainActivity extends Activity {
 
-    Button bak, spela, fram;
+    ImageButton bak, spela, fram;
+    MediaPlayer sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bak = (Button) findViewById(R.id.spela_bak);
-        spela = (Button) findViewById(R.id.spela);
-        fram = (Button) findViewById(R.id.spela_fram);
+        bak = (ImageButton) findViewById(R.id.spela_bak);
+        spela = (ImageButton) findViewById(R.id.spela);
+        fram = (ImageButton) findViewById(R.id.spela_fram);
 
 
-        final MediaPlayer sound = MediaPlayer.create(MainActivity.this, R.raw.music);
+        sound = MediaPlayer.create(MainActivity.this, R.raw.bahar);
+
+
+
+        spela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sound.start();
+
+
+            }
+
+        });
 
         bak.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,13 +44,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        spela.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sound.start();
-            }
 
-        });
 
         fram.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +54,17 @@ public class MainActivity extends Activity {
 
         });
 
+
+        spela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
+
 }
+
