dfc685eac1e996b455e8ad84c4901976896c85dd
adamshafai <shafai86@gmail.com>
2015-08-04 16:26:27 +0200

    MANIFEST CHANGES

--- app/src/main/java/com/shafaisoft/hazaragi/MainActivity.java
+++ app/src/main/java/com/shafaisoft/hazaragi/MainActivity.java
@@ -3,14 +3,16 @@ package com.shafaisoft.hazaragi;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ImageButton;
+
 public class MainActivity extends Activity {
 
-    ImageButton bak, spela, fram;
-    MediaPlayer sound;
+    private ImageButton bak, spela, fram,bakground;
+    private MediaPlayer sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,7 @@ public class MainActivity extends Activity {
         bak = (ImageButton) findViewById(R.id.spela_bak);
         spela = (ImageButton) findViewById(R.id.spela);
         fram = (ImageButton) findViewById(R.id.spela_fram);
-
+        bakground = (ImageButton) findViewById(R.id.bakgrund);
 
         sound = MediaPlayer.create(MainActivity.this, R.raw.bahar);
 
